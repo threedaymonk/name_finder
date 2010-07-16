287d9a4b123282fb8aad08f0d21c4219d17b0172
Paul Battley <pbattley@gmail.com>
2010-07-16 18:41:08 +0100

    Initial commit.

---
+++ lib/name_finder.rb
@@ -0,0 +1,70 @@
+class NameFinder
+  def initialize
+    @root = {}
+  end
+
+  def add(term)
+    NodeProxy.new(@root).add(normalize(term) + " ", term)
+  end
+
+  def find(haystack)
+    each_set_of_words(haystack) do |words|
+      found = NodeProxy.new(@root).find(words + " ")
+      return found if found
+    end
+    nil
+  end
+
+private
+  def each_set_of_words(haystack)
+    words = normalize(haystack).split(/ /)
+    (0 ... words.length).each do |i|
+      yield words[i .. -1].join(" ")
+    end
+  end
+
+  def normalize(term)
+    term.downcase.gsub(/\s+/, " ").gsub(/[^a-z ]+/, "")
+  end
+
+  class NodeProxy
+    def initialize(node)
+      @node = node
+    end
+
+    def add(remainder, term)
+      if remainder
+        head, tail = split_first(remainder)
+        subtree    = @node[tokenize(head)] ||= {}
+        wrap(subtree).add(tail, term)
+      else
+        @node[0] = term
+      end
+    end
+
+    def find(remainder, new_word=false)
+      if remainder
+        head, tail = split_first(remainder)
+        if subtree = @node[tokenize(head)]
+          wrap(subtree).find(tail, head == " ")
+        elsif new_word
+          @node[0]
+        end
+      else
+        @node[0]
+      end
+    end
+
+    def split_first(expression)
+      expression.scan(/^.|.+/)
+    end
+
+    def tokenize(s)
+      s.unpack('U').first
+    end
+
+    def wrap(node)
+      NodeProxy.new(node)
+    end
+  end
+end
---
+++ test/name_finder_test.rb
@@ -0,0 +1,60 @@
+lib_path = File.expand_path("../../lib", __FILE__)
+$:.unshift lib_path unless $:.include?(lib_path)
+require "test/unit"
+require "name_finder"
+
+class NameFinderTest < Test::Unit::TestCase
+
+  def setup
+    @nf = NameFinder.new
+  end
+  attr_reader :nf
+
+  def test_should_find_an_exact_match
+    nf.add "aa bb"
+    assert_equal "aa bb", nf.find("aa bb")
+  end
+
+  def test_should_be_case_insensitive_and_case_preserving
+    nf.add "Aa bb"
+    assert_equal "Aa bb", nf.find("AA BB")
+  end
+
+  def test_should_find_a_substring_match_with_text_before
+    nf.add "aa bb"
+    assert_equal "aa bb", nf.find("xx aa bb")
+  end
+
+  def test_should_find_a_substring_match_with_text_after
+    nf.add "aa bb"
+    assert_equal "aa bb", nf.find("aa bb")
+  end
+
+  def test_should_find_a_substring_match_with_text_before_and_after
+    nf.add "aa bb"
+    assert_equal "aa bb", nf.find("xx aa bb xx")
+  end
+
+  def test_should_return_nil_for_no_match
+    assert_nil nf.find("aa")
+  end
+
+  def test_should_not_find_a_substring_that_does_not_end_on_a_word_boundary
+    nf.add "aa bb"
+    assert_nil nf.find("aa bbb")
+  end
+
+  def test_should_not_find_a_substring_that_does_not_begin_on_a_word_boundary
+    nf.add "aa bb"
+    assert_nil nf.find("aaa bb")
+  end
+
+  def test_should_find_longest_exact_match
+    nf.add "aa"
+    nf.add "aa bb"
+    nf.add "aa bbb"
+    nf.add "aa bbbb"
+    assert_equal "aa bbb", nf.find("xx aa bbb xx")
+  end
+end
+
