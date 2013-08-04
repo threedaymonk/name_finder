de6a4e3f7089577031e17b062a1965b3817a0821
Paul Battley <pbattley@gmail.com>
2013-08-04 01:02:55 +0100

    Group find_in tests

--- test/name_finder_test.rb
+++ test/name_finder_test.rb
@@ -5,51 +5,53 @@ describe NameFinder do
 
   subject { NameFinder.new }
 
-  it "should find an exact match" do
-    subject.add "aa bb"
-    subject.find_in("aa bb").must_equal "aa bb"
-  end
+  describe "find_in" do
+    it "should find an exact match" do
+      subject.add "aa bb"
+      subject.find_in("aa bb").must_equal "aa bb"
+    end
 
-  it "should be case insensitive and case preserving" do
-    subject.add "Aa bb"
-    subject.find_in("AA BB").must_equal "Aa bb"
-  end
+    it "should be case insensitive and case preserving" do
+      subject.add "Aa bb"
+      subject.find_in("AA BB").must_equal "Aa bb"
+    end
 
-  it "should find a substring match with text before" do
-    subject.add "aa bb"
-    subject.find_in("xx aa bb").must_equal "aa bb"
-  end
+    it "should find a substring match with text before" do
+      subject.add "aa bb"
+      subject.find_in("xx aa bb").must_equal "aa bb"
+    end
 
-  it "should find a substring match with text after" do
-    subject.add "aa bb"
-    subject.find_in("aa bb xx").must_equal "aa bb"
-  end
+    it "should find a substring match with text after" do
+      subject.add "aa bb"
+      subject.find_in("aa bb xx").must_equal "aa bb"
+    end
 
-  it "should find a substring match with text before and after" do
-    subject.add "aa bb"
-    subject.find_in("xx aa bb xx").must_equal "aa bb"
-  end
+    it "should find a substring match with text before and after" do
+      subject.add "aa bb"
+      subject.find_in("xx aa bb xx").must_equal "aa bb"
+    end
 
-  it "should return nil for no match" do
-    subject.find_in("aa").must_be_nil
-  end
+    it "should return nil for no match" do
+      subject.find_in("aa").must_be_nil
+    end
 
-  it "should not find_in a substring that does not end on a word boundary" do
-    subject.add "aa bb"
-    subject.find_in("aa bbb").must_be_nil
-  end
+    it "should not find_in a substring that does not end on a word boundary" do
+      subject.add "aa bb"
+      subject.find_in("aa bbb").must_be_nil
+    end
 
-  it "should not find_in a substring that does not begin on a word boundary" do
-    subject.add "aa bb"
-    subject.find_in("aaa bb").must_be_nil
-  end
+    it "should not find_in a substring that does not begin on a word boundary" do
+      subject.add "aa bb"
+      subject.find_in("aaa bb").must_be_nil
+    end
 
-  it "should find longest exact match" do
-    subject.add "aa"
-    subject.add "aa bb"
-    subject.add "aa bbb"
-    subject.add "aa bbbb"
-    subject.find_in("xx aa bbb xx").must_equal "aa bbb"
+    it "should find longest exact match" do
+      subject.add "aa"
+      subject.add "aa bb"
+      subject.add "aa bbb"
+      subject.add "aa bbbb"
+      subject.find_in("xx aa bbb xx").must_equal "aa bbb"
+    end
   end
 
   it "should export and import tree" do
