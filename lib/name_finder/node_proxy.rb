a9b72f5d51b6ac827324949f3d3c064149dc2fb1
Paul Battley <pbattley@gmail.com>
2013-08-04 01:00:07 +0100

    Split NodeProxy to separate file

---
+++ lib/name_finder/node_proxy.rb
@@ -0,0 +1,43 @@
+class NameFinder
+  class NodeProxy
+    def initialize(node)
+      @node = node
+    end
+
+    def add(remainder, term)
+      if remainder
+        head, tail = split_first(remainder)
+        subtree    = @node[tokenize(head)] ||= {}
+        wrap(subtree).add(tail, term)
+      else
+        @node[0] = term
+      end
+    end
+
+    def find(remainder, new_word=false)
+      if remainder
+        head, tail = split_first(remainder)
+        if subtree = @node[tokenize(head)]
+          wrap(subtree).find(tail, head == " ")
+        elsif new_word
+          @node[0]
+        end
+      else
+        @node[0]
+      end
+    end
+
+  private
+    def split_first(expression)
+      expression.scan(/^.|.+/)
+    end
+
+    def tokenize(s)
+      s.unpack('U').first
+    end
+
+    def wrap(node)
+      NodeProxy.new(node)
+    end
+  end
+end
